0046d284e0e1285a5bfa8a5206d4d4ff6741c7e4
Benjamin Basuki <benyhutama16@go.byuh.edu>
2026-06-29 15:43:06 -1000

    Add chatbot embed script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,6 +29,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      <Script
+        id="chatbot-embed"
+        src="https://10.11.240.55/embed.js"
+        strategy="afterInteractive"
+      />
     </html>
   );
 }
